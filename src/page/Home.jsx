469effa4983fccc2c066148c5f8c9d3bb007a637
Mahroof VP <mahroofvp77@gmail.com>
2024-09-30 21:53:52 +0530

    bg chart modified1

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -8,13 +8,13 @@ const Home = () => {
 
         <svg className="w-full h-full" viewBox="0 0 800 600">
           <path
-            d="M 0 500 L 50 400 L 100 450 L 150 350 L 200 400 L 250 300 L 300 350 L 350 300 L 400 350 L 450 200 L 500 250 L 550 150 L 600 200"
+            d="M 100 450 L 150 350 L 200 400 L 250 300 L 300 350 L 350 300 L 400 350 L 450 200 L 500 250 L 550 150 L 600 200 L 650 100"
             stroke="yellow"
             strokeWidth="4"
             fill="none"
           />
           <g stroke="#ffffff">
-            <line x1="50" y1="420" x2="50" y2="560" />
+            <line x1="600" y1="100" x2="600" y2="300" />
             <line x1="150" y1="400" x2="150" y2="300" />
             <line x1="250" y1="350" x2="250" y2="500" />
             <line x1="350" y1="200" x2="350" y2="400" />
@@ -27,7 +27,7 @@ const Home = () => {
         <h1 className="text-5xl md:text-7xl font-medium text-yellow-600 mb-2 font-serif">
           CHARTBY 
         </h1>
-        <h3 className="text-3xl md:text-5xl font-extralight text-white mb-2 journal-text">
+        <h3 className="journal-text text-1xl  md:text-4xl lg:text-5xl font-extralight text-white mb-2 ">
           TRADING JOURNAL
         </h3>
         <div className="w-auto md:w-auto h-1 bg-slate-200 mb-4"></div>
